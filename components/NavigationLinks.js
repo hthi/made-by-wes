97fc3d6a98ac9f4114aba976ec2172c2ef75c33e
Helen Thi <helen.thi@cengage.com>
2017-12-26 09:13:28 -0500

    add hamburger nav menu

--- components/NavigationLinks.js
+++ components/NavigationLinks.js
@@ -1,28 +1,61 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NavigationLinks = () => {
-  const curElement = document.activeElement;
-  const links = document.getElementsByClassName('nav-link');
+export default class NavigationLinks extends React.Component {
 
-  for (let x = 0; x < links.length; x++) {
-    if (links[x] === curElement) {
-      links[x].parentElement.classList.add("active");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showHamburger: false
+    };
+  };
+
+  toggleMenu() {
+    if (this.state.showHamburger && this.state.showHamburger === true) {
+      this.setState({
+        showHamburger: false
+      });
     } else {
-      links[x].parentElement.classList.remove("active");
+      this.setState({
+        showHamburger: true
+      });
     }
   }
 
-  return (
-    <div className="links-container">
-      <ul>
-        <li className="active" key='home'><Link to='/' className="nav-link">HOME</Link></li>
-        <li key='about'><Link to='/about' className="nav-link">ABOUT</Link></li>
-        <li key='portfolio'><Link to='/portfolio' className="nav-link">PORTFOLIO</Link></li>
-        <li key='contact'><Link to='/contact' className="nav-link">CONTACT</Link></li>
-      </ul>
-    </div>
-  );
-};
+  render() {
+    const hamburgerClass = this.state.showHamburger ? "" : "hide";
+    const curElement = document.activeElement;
+    const links = document.getElementsByClassName('nav-link');
 
-export default NavigationLinks;
+    for (let x = 0; x < links.length; x++) {
+      if (links[x] === curElement) {
+        links[x].parentElement.classList.add("active");
+      } else {
+        links[x].parentElement.classList.remove("active");
+      }
+    }
+
+    return (
+      <div>
+        <div className="links-container">
+          <ul>
+            <li className="active" key='home'><Link to='/' className="nav-link">HOME</Link></li>
+            <li key='about'><Link to='/about' className="nav-link">ABOUT</Link></li>
+            <li key='portfolio'><Link to='/portfolio' className="nav-link">PORTFOLIO</Link></li>
+            <li key='contact'><Link to='/contact' className="nav-link">CONTACT</Link></li>
+          </ul>
+        </div>
+        <div className="hamburger-menu" onClick={() => this.toggleMenu()}>
+          <i className="fa fa-bars" aria-hidden="true"/>
+          <ul className={hamburgerClass}>
+            <li className="active" key='home'><Link to='/' className="nav-link">HOME</Link></li>
+            <li key='about'><Link to='/about' className="nav-link">ABOUT</Link></li>
+            <li key='portfolio'><Link to='/portfolio' className="nav-link">PORTFOLIO</Link></li>
+            <li key='contact'><Link to='/contact' className="nav-link">CONTACT</Link></li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+};
