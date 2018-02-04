9bbd7b8491a51dd74bbf037d4ebe085b56f1f6a8
Helen Thi <helen.thi@cengage.com>
2018-02-04 13:13:12 -0500

    center projects

--- components/Projects.js
+++ components/Projects.js
@@ -5,8 +5,8 @@ const Portfolio = () => (
   <div className="content-container">
     <h2>Check out some of my projects!</h2>
     <div className="projects-container">
-      <div key='project1'><Link to='/portfolio/project1'><img className="project-img" src="../assets/project_1.0.jpg"/></Link></div>
-      <div key='project2'><Link to='/portfolio/project2'><img className="project-img" src="../assets/project_2.0.jpg"/></Link></div>
+      <div className="project-container" key='project1'><Link to='/portfolio/project1'><img className="project-img" src="../assets/project_1.0.jpg"/></Link></div>
+      <div className="project-container" key='project2'><Link to='/portfolio/project2'><img className="project-img" src="../assets/project_2.0.jpg"/></Link></div>
     </div>
   </div>
 );
