ebde0131407a3e8500aefa0e0fff1d47677d11c8
Helen Thi <helenthi@Helen-This-MacBook-Pro.local>
2018-10-27 16:31:25 -0400

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-  entry: "./index.js",
-  output: {
-    path: __dirname,
-    filename: "./bundle.js"
-  },
+    entry: "./index.js",
+    output: {
+      path: __dirname,
+      filename: "./bundle.js"
+    },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        use: {
+          loader: "babel-loader"
         }
       },
       {
